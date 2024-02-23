5682e051927c8296be114458bc73d5ff606dd8c3
Manisha1107 <manisapkhrl@gmail.com>
2024-02-23 15:21:21 +0545

    feat:button feature added

---
+++ script.js
@@ -0,0 +1,21 @@
+function generate(){
+
+const quotes = {
+    science: [
+      "The science of today is the technology of tomorrow. - Edward Teller",
+      "Science is a way of thinking much more than it is a body of knowledge. - Carl Sagan",
+      "The good thing about science is that it's true whether or not you believe in it. - Neil deGrasse Tyson"
+    ],
+    others: [
+      "The only way to do great work is to love what you do. - Steve Jobs",
+      "Believe you can and you're halfway there. - Theodore Roosevelt",
+      "The future belongs to those who believe in the beauty of their dreams. - Eleanor Roosevelt"
+    ]
+   
+  };
+  const author = Object.keys(quotes);
+
+  
+  console.log(author)
+  
+}
