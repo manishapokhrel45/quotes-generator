0d4a40c22759f9405c053adb58353753c4f2f583
Manisha1107 <manisapkhrl@gmail.com>
2024-02-24 11:23:51 +0545

    feat:button enabled

--- script.js
+++ script.js
@@ -1,22 +1,43 @@
 
 
 const quotes = {
-    science: [
+    Science: [
       "The science of today is the technology of tomorrow. - Edward Teller",
       "Science is a way of thinking much more than it is a body of knowledge. - Carl Sagan",
       "The good thing about science is that it's true whether or not you believe in it. - Neil deGrasse Tyson"
     ],
-    others: [
+    Inspiration: [
       "The only way to do great work is to love what you do. - Steve Jobs",
       "Believe you can and you're halfway there. - Theodore Roosevelt",
       "The future belongs to those who believe in the beauty of their dreams. - Eleanor Roosevelt"
     ]
    
   };
-  function generate(){
-  const author = Object.keys(quotes);
+  let category="Science";
+  let index=0;
+  function generateQuote(){
+    document.querySelector("#quote").innerHTML= quotes[category][index]
+  }
+  function nextQuote(){
+    index = (index + 1) % quotes[category].length;
+    generateQuote()
+  }
+  function previousQuote(){
+    index = (index - 1) % quotes[category].length;
+    generateQuote()
+  }
+  function randomQuote(){
 
   
-  console.log(author)
+  index = Math.floor(Math.random() * quotes[category].length);
+  generateQuote();
+  }
+  function updateCategory(){
+    category = "Inspiration";
+    index = 0;
+    generateQuote();
+
+  }
   
-}
+ 
+  
